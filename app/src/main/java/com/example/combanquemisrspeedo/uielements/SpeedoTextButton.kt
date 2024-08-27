5d4ce1d26e9ccb369f73a0b287ef22069566b389
mayers22 <mayersoliman4@gmail.com>
2024-08-27 08:09:30 +0300

    ButtonItem By Dina

---
+++ app/src/main/java/com/example/combanquemisrspeedo/uielements/SpeedoTextButton.kt
@@ -0,0 +1,42 @@
+package com.example.combanquemisrspeedo.uielements
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SpeedoTextButton(text: String, textColor: Color, backgroundColor: Color,borderColor: Color) {
+    TextButton(
+        onClick = {},
+        modifier = Modifier
+            .height(51.dp)
+            .width(343.dp)
+            .border(
+                width = 1.5.dp,
+                color = borderColor,
+                shape = RoundedCornerShape(6.dp)
+            )
+            .background(
+                color = backgroundColor,
+                shape = RoundedCornerShape(6.dp))
+    ) {
+        Text(
+            text,
+            color = textColor,
+            fontSize = 16.sp,
+            lineHeight = 20.8.sp,
+            fontWeight = FontWeight(500),
+        )
+    }
+
+}
