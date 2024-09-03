1fb181b056a9a824f9a9d3142f67490f7c3215c7
mayers22 <mayersoliman4@gmail.com>
2024-09-03 19:40:27 +0300

    Profile

--- app/src/main/java/com/example/combanquemisrspeedo/profile/SettingsScreen.kt
+++ app/src/main/java/com/example/combanquemisrspeedo/profile/SettingsScreen.kt
@@ -1,32 +1,22 @@
 package com.example.combanquemisrspeedo.profile
 
-import androidx.compose.foundation.Image
+
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -36,13 +26,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.combanquemisrspeedo.R
-import com.example.combanquemisrspeedo.uielements.SpeedoTextButton
-import com.example.combanquemisrspeedo.uielements.SpeedoTextField
-import edu.android_security.ui.theme.G100
+import com.example.combanquemisrspeedo.uielements.SettingsItem
 import edu.android_security.ui.theme.G900
-import edu.android_security.ui.theme.P
-import edu.android_security.ui.theme.P50
-import edu.android_security.ui.theme.White
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -110,73 +95,12 @@ fun SettingsScreen(modifier: Modifier = Modifier, onBackClick: () -> Unit) {
 
                     }
                 )
+                HorizontalDivider()
 
             }
         }
     )
 }
-@Composable
-fun SettingsItem(
-    icon: Int,
-    title: String,
-    description: String,
-    onClick: () -> Unit,
-    onArrowClick: () -> Unit
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(onClick = onClick)
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Box(
-            modifier = Modifier
-                .size(48.dp)
-                .clip(RoundedCornerShape(12.dp))
-                .background(P50),
-            contentAlignment = Alignment.Center
-        ) {
-            Image(
-                painter = painterResource(id = icon),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(24.dp)
-
-            )
-        }
-
-        Spacer(modifier = Modifier.width(16.dp))
-
-        Column(
-            modifier = Modifier.weight(1f)
-        ) {
-            Text(
-                color = G900,
-                fontSize = 16.sp,
-                text = title,
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-            Text(
-                text = description,
-                color = G100,
-                fontSize = 16.sp,
-            )
-        }
-
-        IconButton(
-            onClick = onArrowClick,
-            modifier = Modifier.size(24.dp)
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.arrow_right),
-                contentDescription = "Navigate"
-            )
-        }
-    }
-}
-
 
 @Preview
 @Composable
