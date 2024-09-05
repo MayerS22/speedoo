020d3741e6579544ebaa91cfd079d3dce8f9affc
dinabdullah <dina_abdullah04@yahoo.com>
2024-09-05 23:55:29 +0300

    TransferPackage

---
+++ app/src/main/java/com/example/combanquemisrspeedo/transfer/FavouriteListSheet.kt
@@ -0,0 +1,80 @@
+package com.example.combanquemisrspeedo.transfer
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.combanquemisrspeedo.R
+import com.example.combanquemisrspeedo.uielements.FavouriteListItem
+import com.example.combanquemisrspeedo.uielements.SpeedoTextButton
+import com.example.combanquemisrspeedo.uielements.SpeedoTextField
+import edu.android_security.ui.theme.P300
+import edu.android_security.ui.theme.White
+
+@Composable
+fun FavouriteListSheet(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .height(515.dp)
+            .padding(16.dp)
+            .padding(bottom = 100.dp)
+    ) {
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.favorite),
+                contentDescription = "",
+                tint = P300
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = "Favorite",
+                fontWeight = FontWeight.W400,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        FavouriteListItem(
+            name = "Asmaa Dosuky",
+            account = "xxxx7890",
+            onclickDelete = {},
+            onclickEdit = {}
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        FavouriteListItem(
+            name = "Asmaa Dosuky",
+            account = "xxxx7890",
+            onclickDelete = {},
+            onclickEdit = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun FavouriteListSheetPreview() {
+    FavouriteListSheet()
+}
